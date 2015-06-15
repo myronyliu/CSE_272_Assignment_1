664a24a2722003820997e1e3dc9df655b953e01a
yshmnks <myronyliu@gmail.com>
2015-06-14 18:18:45 -0700

    took out the 4 corner check in unified and bidiPath tracing, since we aren't doing jittered sampling anyways

--- hw1/miro/Scene.cpp
+++ hw1/miro/Scene.cpp
@@ -362,14 +362,8 @@ void Scene::biditraceImage(Camera *cam, Image *img) {
     {
         for (int x = 0; x < w; x++)
         {
-            Ray ray00 = cam->eyeRay((float)x - 0.5, (float)y - 0.5, img->width(), img->height());
-            Ray ray01 = cam->eyeRay((float)x - 0.5, (float)y + 0.5, img->width(), img->height());
-            Ray ray10 = cam->eyeRay((float)x + 0.5, (float)y - 0.5, img->width(), img->height());
-            Ray ray11 = cam->eyeRay((float)x + 0.5, (float)y + 0.5, img->width(), img->height());
-            if (!trace(hitInfo, ray00) &&
-                !trace(hitInfo, ray01) &&
-                !trace(hitInfo, ray10) &&
-                !trace(hitInfo, ray11)) continue;
+            Ray ray = cam->eyeRay(x, y, img->width(), img->height());
+            if (!trace(hitInfo, ray)) continue;
             Vector3 fluxSumOverSamples(0, 0, 0);
 
             Concurrency::parallel_for(0, m_bidiSamplesPerPix, [&](int k) {
@@ -584,7 +578,7 @@ Vector3 Scene::uniRadiance(const int& i, const int& j, const LightPath& lightPat
 
         if (k == i) {
             if (explicitConnection == true) flux *= probPI;
-            else flux *= probDE;
+            else flux *= probDE; // the division by nLightPaths is implicitly done here
         }
 
         probSum += probPI + probDE*nLightPaths;
@@ -606,12 +600,8 @@ Vector3 Scene::uniRadianceDE(const int& j, const EyePath& eyePath, PhotonMap* ph
     for (int k = 0; k < photons.size(); k++) density += photons[k].m_power;
     density /= diskArea;
 
-    //cout << density[0] << endl;
-
     Vector3 flux(0,0,0);
-
-    int smallPhotons = 0;
-
+    
     for (auto & photon : photons) {
         int i = photon.m_hitIndex;
         // usually with explicit connection we would make i = photon.m_hitIndex +1
@@ -620,9 +610,6 @@ Vector3 Scene::uniRadianceDE(const int& j, const EyePath& eyePath, PhotonMap* ph
         LightPath lightPath = *photon.m_lightPath;
         Vector3 fluxAccum = uniRadiance(i, j, lightPath, eyePath, photonMap, false, nLightPaths, density);
         flux += fluxAccum;
-        if (fluxAccum[0] < 0.000001) {
-            smallPhotons++;
-        }
         //return flux;
     }
     //cout << smallPhotons << "/" << photons.size() << endl;
@@ -646,19 +633,11 @@ Scene::unifiedpathtraceImage(Camera *cam, Image *img) {
         //for (int x = w / 2 - 1; x < w / 2 + 1; x++)
         for (int x = 0; x < w; x++)
         {
-            Ray ray00 = cam->eyeRay((float)x - 0.5, (float)y - 0.5, img->width(), img->height());
-            Ray ray01 = cam->eyeRay((float)x - 0.5, (float)y + 0.5, img->width(), img->height());
-            Ray ray10 = cam->eyeRay((float)x + 0.5, (float)y - 0.5, img->width(), img->height());
-            Ray ray11 = cam->eyeRay((float)x + 0.5, (float)y + 0.5, img->width(), img->height());
-            if (!trace(hitInfo, ray00) &&
-                !trace(hitInfo, ray01) &&
-                !trace(hitInfo, ray10) &&
-                !trace(hitInfo, ray11)) continue;
+            Ray ray = cam->eyeRay(x, y, img->width(), img->height());
+            if (!trace(hitInfo, ray)) continue;
             Vector3 fluxSumOverSamples(0, 0, 0);
 
-            /*Ray ray = cam->eyeRay(x, y, img->width(), img->height());
-            trace(hitInfo, ray00);
-            vector<PhotonDeposit> photons = mapAndPaths.first->getPhotons(hitInfo.P, m_photonGatheringRadius);
+            /*vector<PhotonDeposit> photons = mapAndPaths.first->getPhotons(hitInfo.P, m_photonGatheringRadius);
             Vector3 density(0, 0, 0);
             for (int k = 0; k<photons.size(); k++) {
                 density += photons[k].m_power;
